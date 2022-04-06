853a3692ac650da6ec1b24f76edd7649850b5784
Lucas Brandão <lucas.brandao@hanzo.com.br>
2022-04-06 01:47:59 -0300

    token fix

--- Helper/Constants.swift
+++ Helper/Constants.swift
@@ -11,8 +11,7 @@ struct APIConstants {
     static let baseEndpoint = "https://ott-details.p.rapidapi.com"
     
     //MARK: Insert your RapidAPI token here
-    static let token = "64851682d2msh2bc59d76775ca15p1ffd22jsn31e638b59081"
-//    static let token = "55f456827amsh4a6dcc5baa80a42p15aa28jsn7c07d67c1b64
+    static let token = ""
 
 }
 
